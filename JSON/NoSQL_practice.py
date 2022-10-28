85bee0c3b28ad2da6b684abedcae215f2bf2caa4
mikestantonmusic <62892791+mikestantonmusic@users.noreply.github.com>
2022-10-27 22:51:40 -0500

    Cleaned up Code

--- JSON/NoSQL_practice.py
+++ JSON/NoSQL_practice.py
@@ -5,11 +5,8 @@ import pandas as pd
 import NoSQL_functions
 from NoSQL_functions import *
 
-import os
 os.chdir('JSON/')
 print('current working directory:', os.getcwd())
-
-import json
 
 with open('data/Sample Json with 200 Records.json',encoding="utf-8") as f:
    data1 = json.load(f)
